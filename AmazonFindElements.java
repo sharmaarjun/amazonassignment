be44e5ac6c08532a8668d6116b7bbd8f7f6bcef7
sharmaarjun <33047630+sharmaarjun@users.noreply.github.com>
2021-02-14 22:56:42 +0530

    Screenshots included

--- AmazonFindElements.java
+++ AmazonFindElements.java
@@ -2,6 +2,8 @@ package com.ecommerce.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
@@ -14,9 +16,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 
 import junit.framework.Assert;
 
@@ -72,29 +77,31 @@ class AmazonFindElements {
 		details.click();
 	}
 	@Test
-	public void addToCart() {
+	public void addToCart() throws InterruptedException, IOException {
 		String siteUrl = "https://www.amazon.in/";
 		driver.get(siteUrl);
 		WebElement searchBox1 = driver.findElement(By.id("twotabsearchtextbox"));
 		searchBox1.sendKeys("iPhone 12");
 		driver.findElement(By.id("nav-search-submit-button")).click();
-	}
-		/*
-		//Switch Tabs
-		String oldTab = driver.getWindowHandle();
-		//new tab is opened on-click
-	    driver.findElement(By.cssSelector("#search > div.s-desktop-width-max.s-desktop-content.sg-row > div.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(2) > div > span > div > div > div > div > div:nth-child(2) > div.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.sg-col > div > div:nth-child(1) > div > div > div:nth-child(1) > h2 > a > span")).click();
-	    ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
-	    newTab.remove(oldTab);
-	    // change focus to new tab
-	    driver.switchTo().window(newTab.get(0));
-		WebElement sub = driver.findElement(By.id("add-to-cart-button"));
-		sub.click();
-		WebElement cart = driver.findElement(By.xpath("//*[@id=\"attach-sidesheet-view-cart-button\"]/span/input"));
-		cart.click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[2]/div[2]/div/span/div/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span")).click();
+		Thread.sleep(2000);
+		//get window handle
+		String mainWindow = driver.getWindowHandle();
+		ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(newTab.get(1));
+		driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
+		Thread.sleep(2000);
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:\\Users\\Arjun Sharma\\eclipse-workspace\\phase1-selenium-scripting\\src\\com\\ecommerce\\tests\\Screenshots\\addtocart.png"));
+		
 		driver.close();
-		driver.switchTo().window(oldTab);
-		*/
+		driver.switchTo().window(newTab.get(0));
+		
+	}
+	
 	@Test
 	public void viewCart() {
 		String siteUrl = "https://www.amazon.in/";
@@ -104,7 +111,7 @@ class AmazonFindElements {
 	}
 	
 	@Test
-	public void addAddress() throws InterruptedException {
+	public void addAddress() throws InterruptedException, IOException {
 		String siteUrl = "https://www.amazon.in/";
 		driver.get(siteUrl);
 		Thread.sleep(5000);
@@ -123,9 +130,13 @@ class AmazonFindElements {
 			driver.findElement(By.cssSelector("#GLUXZipUpdateInput")).sendKeys("134113");
 			driver.findElement(By.cssSelector("#GLUXZipUpdate > span > input")).click();
 			Thread.sleep(2000);
+			TakesScreenshot ts = (TakesScreenshot) driver;
+			File src = ts.getScreenshotAs(OutputType.FILE);
+			FileHandler.copy(src, new File("C:\\Users\\Arjun Sharma\\eclipse-workspace\\phase1-selenium-scripting\\src\\com\\ecommerce\\tests\\Screenshots\\address.png"));
+			
 			driver.close();
 		}
-		driver.switchTo().window(mainWindow);
+		
 	}
 	
 	@Test
@@ -137,7 +148,7 @@ class AmazonFindElements {
 		//driver.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[2]/a")).click();
 	}
 	@Test
-	public void signIn() {
+	public void signIn() throws IOException {
 		String siteUrl = "https://www.amazon.in/";
 		driver.get(siteUrl);
 		driver.findElement(By.id("nav-link-accountList")).click();
@@ -145,6 +156,10 @@ class AmazonFindElements {
 		driver.findElement(By.id("continue")).click();
 		driver.findElement(By.id("ap_password")).sendKeys("qwerty");
 		driver.findElement(By.id("signInSubmit")).click();
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:\\Users\\Arjun Sharma\\eclipse-workspace\\phase1-selenium-scripting\\src\\com\\ecommerce\\tests\\Screenshots\\signin.png"));
+		
 	}
 
 }
